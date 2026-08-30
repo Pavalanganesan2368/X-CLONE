2d9d97925fa598d713cd11df87a2ae2f400c8394
Pavalanganesan2368 <pavalanganesan2368@gmail.com>
2026-08-30 12:32:23 +0530

    Project Completed

--- frontend/src/Pages/Auth/LoginPage.jsx
+++ frontend/src/Pages/Auth/LoginPage.jsx
@@ -32,6 +32,7 @@ const LoginPage = () => {
 				const data = await response.json();
 				if (!response.ok) throw new Error(data.error || "Something Went Wrong");
 
+				return data;
 			} catch (error) {
 				throw error
 			}
