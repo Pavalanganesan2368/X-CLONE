5a2271edda6245af184ad32fbe5bf36e7a321bf7
Pavalanganesan2368 <pavalanganesan2368@gmail.com>
2026-08-30 12:03:00 +0530

    Project Completed

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
 app.use(cors({
-    origin : "http://localhost:5173",
+    origin : "https://twitter-x-yvvz.onrender.com",
     credentials : true
 }));
 
